237259dd008ce46bfc41a7df1d1e219b9cbae40d
Soumita Nag A <soumita.a.nag@ericsson.com>
2020-02-25 19:19:08 +0530

    readEmail.py

---
+++ test.py
@@ -0,0 +1,134 @@
+import calendar
+import os
+import win32com.client
+#print 
+print("hello")
+print("world")
+#if 1==0:    
+ #   print("world")
+#print("byee")
+#add user input
+#a=input("enter value :")
+# print(a)
+
+#print type of input
+# num=input("enter number : ")
+# print(type(num))
+# x=num*num
+# print(x)
+
+#power
+# a=2
+# print(a**3)
+
+#loop
+# num1=int(input("Enter the 1st number : "))
+# num2=int(input("Enter the 2nd number : "))
+# operation =int(input("enter 1 for add, 2 for sub, 3 for mul, 4 for div: "))
+
+# if operation ==1:
+#     print(num1+num2)
+# elif operation ==2:
+#     print(num1-num2)
+# elif operation ==3:
+#     print(num1*num2)
+# elif operation ==4:
+#     print(num1/num2)
+# else:
+#     print("Invalid Input")
+
+#concatination
+# name = "soumita"
+# surname = "Nag"
+# print(name+surname)
+
+#slice
+# msg = "Welcome to ericsson"
+# print (msg[0:2])
+
+# if 'e' in msg:
+#     print("true")
+
+#strip
+
+#joining
+
+#filter
+# url="https://www.ess.adp.in/ESS4/ESSV5/incometax/submission"
+# x- url.split("?")[1]
+# y = x.split("&")
+
+#list
+# a=[2,4,1,5]
+# new_list=[]
+# for i in a:
+#     if i%2==0:
+#         new_list.append(i)
+
+# print("value of new list",new_list)
+
+#range
+# a=range(10)
+# print(a)
+# print(list(a))
+
+#dictionary
+# dic={1:"one", 2:"two", "three":3}
+# print(dic[2])
+
+# for key, value in dic.items():
+#     print(key,value)
+
+#calendar
+# print(calendar.month(2020,2))
+
+#method
+# def addition(x,y):
+#     o=x+y
+#     print(o)
+#     return (o)
+
+# def check():
+#     ad_o = addition(2,4)
+
+
+# check()
+
+#file parsing
+# file = open("C:\Soumita\workspace\Python\sample.txt")
+# print(file.readlines())
+
+# for i in file.readlines():
+#     if "CPython" in i:
+#         print(i)
+
+#os
+# print(os.getcwd()+"\sample.txt")
+
+#read email
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
+                                    # the inbox. You can change that number to reference
+                                    # any other folder
+messages = inbox.Items
+message = messages.GetLast()
+body_content = message.Body
+print (body_content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
